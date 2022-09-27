44b6ffb57a227050063ba55e61069eac08981855
Lmcardenas98 <lmcardenas.981106@gmail.com>
2022-09-27 01:34:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import separator from './images/pattern-divider-desktop.svg';
+import btn from './images/icon-dice.svg';
 
 function App() {
+
+  const [random, setRandom] = useState(1)
+  const [api, setApi] = useState([])
+
+  useEffect(() => {
+    fetch(`https://api.adviceslip.com/advice/${random}}`)
+                          .then(resp => resp.json())
+                          .then(resp => setApi(resp.slip))
+  }, [random])
+  console.log(random)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ADVICE #{api.id}</h1>
+      <p>"{api.advice}"</p>
+      <div className='separator'>
+        <img src={separator} alt='' />
+      </div>
+      <div className='container-btn'>
+        <div className='btn' onClick={() => setRandom(random+1)}>
+          <img src={btn}  alt=''/>
+        </div>
+      </div>
     </div>
   );
 }
